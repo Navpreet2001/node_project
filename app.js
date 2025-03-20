3cd3b5843d891b5c33f6c77484adee2f06084502
Navpreet <navpreetkaurbb25@gmail.com>
2025-03-20 11:16:18 -0600

    update

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+require('dotenv').config();
+const PORT = process.env.PORT;
+const app = express();
+
+//setup our view engine
+app.set('view engine', 'ejs');
+app.set('views', "./views");
+
+app.use(express.static(`public`));
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Connected to port: ${PORT}`);
+});
